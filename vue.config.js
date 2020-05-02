b0fd76c06b4079a0735814b12ef7eff0188afca4
Linosau <liuf@liuf.com>
2020-05-02 18:13:43 +0800

    config travis ci

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ const resolve = dir => {
   return path.join(__dirname, dir)
 }
 
-const BASE_URL = process.env.NODE_ENV === 'production' ? './' : '/'
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/vue-chicken-music' : '/'
 
 module.exports = {
   publicPath: BASE_URL,
